7ae6020b2b43a203668706ee3b6e7ffc14918790
Rajasekar Elango <e.rajasekar@gmail.com>
2014-04-04 06:41:31 +0800

    Grid layout POC working

--- src/astrochart.js
+++ src/astrochart.js
@@ -4,10 +4,41 @@
 
 function drawChart() {
     s = Snap(800, 600);
+    var cellWidth = 100;
+    var cellHeight = 100;
 
-    s.rect(0, 0, 100, 100).attr({
+    s.rect(0, 0, cellWidth, cellHeight).attr({
         fill: 'white',
         stroke: 'black'
     });
-    s.text(5, 10, "Sun");
+    var planets = ["Su", "Mo", "Ma", "Me", "Ju", "Ve" , "Sa", "Ra", "Ke"];
+
+
+    var x = 5;
+    var y = 20;
+    var xspacing = cellWidth / 3;
+    var yspacing = cellHeight / 3;
+
+    for (var i = 0; i < planets.length; ++i) {
+        var planet = planets[i];
+
+        var point = findLocation({x:x, y:y, width:cellWidth, height:cellHeight, gridPosition:i})
+        s.text(point.x, point.y, planet);
+    }
+}
+
+function findLocation(cell){
+    var x = cell.x;
+    var y = cell.y;
+    var cellWidth = cell.width;
+    var cellHeight = cell.height;
+    var gridPosition = cell.gridPosition;
+    var xMultiplier = Math.floor(gridPosition / 3 );
+    var yMultiplier = (gridPosition % 3 );
+    var gridWidth = cellWidth / 3;
+    var gridHeight = cellHeight / 3;
+    var point = {x:(xMultiplier * gridWidth) + x, y:(yMultiplier * gridHeight) + y};
+    console.log(point);
+    return point;
+
 }
