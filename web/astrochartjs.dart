1ec958262ec282a356e6bfe08cd83ba3a7dd0040
Rajasekar Elango <e.rajasekar@gmail.com>
2014-04-10 18:17:26 -0400

    Removed dart project files

--- web/astrochartjs.dart
+++
@@ -1,9 +0,0 @@
-import 'package:js/js.dart' as js;
-
-void main() {
-  var s = js.context.Snap(800, 600);
-      var cellWidth = 100;
-      var cellHeight = 100;
-
-      s.rect(0, 0, cellWidth, cellHeight);
-}
